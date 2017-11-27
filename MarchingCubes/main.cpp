5368160594a0b7652bf9076e0d225307a750a651
Mauro Amarante <mauro.amarante94@gmail.com>
2017-11-27 00:10:48 -0600

    Initial Commit

---
+++ MarchingCubes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MarchingCubes
+//
+//  Created by Mauro Amarante on 11/27/17.
+//  Copyright © 2017 Mauro Amarante. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
